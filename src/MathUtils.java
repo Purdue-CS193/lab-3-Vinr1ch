5c4568786417c805218d26d1a5480808594e2d53
vinrich <colin@vinarcik.com>
2018-09-26 14:40:48 -0400

    finished the homework

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,11 +3,11 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(double dividend, double divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
+        double hi = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
-        return res;
+        return hi;
     }
 
     /**
@@ -28,12 +28,12 @@ public class MathUtils {
         /* Multiply the result by the base exp times
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
-        for (int i = 0; i < exp; i++) {
-           exp *= base;
+        for (int i = 0; i < exp-1; i++) {
+           res *= base;
         }
 
         /* If it's a negative exponent, we should invert it! */
-        if (isNegative) {
+        if (isNegative == true) {
             double dividedRes = divideNumbers(1, res);
             return dividedRes;
         }
